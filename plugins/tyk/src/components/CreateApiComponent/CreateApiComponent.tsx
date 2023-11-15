82e32cb5ee6ef3d67930d1e86c42a619805bb9e7
David Garvey <davegarvey@hotmail.com>
2023-11-15 11:10:10 +0100

    async fetch works

--- plugins/tyk/src/components/CreateApiComponent/CreateApiComponent.tsx
+++ plugins/tyk/src/components/CreateApiComponent/CreateApiComponent.tsx
@@ -6,9 +6,20 @@ import { Button, Card, TextField } from "@material-ui/core";
 import { useApi, configApiRef } from '@backstage/core-plugin-api';
 
 export const CreateApiComponent = () => {
+  const config = useApi(configApiRef);
+  const backendUrl = config.getString('backend.baseUrl');
+
   const handleClick = async () => {
-    const a = 1;
-    // const response = await fetch('http://localhost:3000/api/apis', { headers: { Authorization: '7304f7eb99984e9f6837d3f8bbc7e252' } })
+    try {
+      const response = await fetch(backendUrl + '/api/proxy/tyk/apis');
+
+      // const response = await fetch('http://localhost:3000/api/apis', { headers: { Authorization: '7304f7eb99984e9f6837d3f8bbc7e252' } });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+    // const response = await fetch()
 
     // const data = await response.json()
     // const apis = data.apis
