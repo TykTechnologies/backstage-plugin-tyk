f602b8729ce73eac35c44dc26860b1a3f1813be1
David Garvey <davegarvey@hotmail.com>
2023-11-15 12:36:55 +0100

    add api to tyk

--- plugins/tyk/src/components/CreateApiComponent/CreateApiComponent.tsx
+++ plugins/tyk/src/components/CreateApiComponent/CreateApiComponent.tsx
@@ -1,41 +1,57 @@
 import React from 'react';
-import { Table, TableColumn, Progress, ResponseErrorPanel, LinkButton } from '@backstage/core-components';
-// import {fetchApiRef, useApi} from '@backstage/core-plugin-api';
-import useAsync from 'react-use/lib/useAsync';
 import { Button, Card, TextField } from "@material-ui/core";
 import { useApi, configApiRef } from '@backstage/core-plugin-api';
+import { kebabCase } from 'lodash'
 
 export const CreateApiComponent = () => {
   const config = useApi(configApiRef);
-  const backendUrl = config.getString('backend.baseUrl');
+  const backendUrl = config.getString('backend.baseUrl') + '/api/proxy/tyk/apis';
 
-  const handleClick = async () => {
+  const handleClick = async (event) => {
+    event.preventDefault();
+    console.log(event);
+    const apiNameValue = event.target.apiname.value;
+    const kebabApiNameValue = kebabCase(apiNameValue);
+
     try {
-      const response = await fetch(backendUrl + '/api/proxy/tyk/apis');
+      const response = await fetch(backendUrl, {
+        method: 'POST',
+        body: JSON.stringify({
+          api_definition: {
+            name: apiNameValue,
+            use_keyless: true,
+            version_data: {
+              not_versioned: true,
+              versions: {}
+            },
+            proxy: {
+              listen_path: `/${kebabApiNameValue}/`,
+              target_url: 'http://httpbin/',
+              strip_listen_path: true
+            },
+            active: true
+          }
+        })
+      })
 
-      // const response = await fetch('http://localhost:3000/api/apis', { headers: { Authorization: '7304f7eb99984e9f6837d3f8bbc7e252' } });
       const data = await response.json();
-      console.log(data);
+      if (response.status != 200) {
+        alert('Error adding API to Tyk - see console');
+        console.error(data);
+      } else {
+        alert('API added to Tyk');
+      }
     } catch (error) {
       console.error(error);
     }
-    // const response = await fetch()
-
-    // const data = await response.json()
-    // const apis = data.apis
-
-    // apis.forEach((api: { api_definition: { api_id: any; name: any; }; }) => {
-    //   console.log('a');
-    // });
-    alert('Your click worked!');
   };
 
   return (
     <Card>
       <h1>Create API</h1>
-      <form>
-        <TextField id="api-name" label="API Name" variant="standard" />
-        <Button id="create-api" variant="contained" onClick={handleClick}>Create</Button>
+      <form onSubmit={handleClick}>
+        <TextField id="apiname" name="apiname" label="API Name" variant="standard" />
+        <Button type="submit" id="create-api" variant="contained">Create</Button>
       </form>
     </Card>
   );
